0a9958905e3f031112689894e4be5fd888837621
ahviplc <ahlc@sina.cn>
2021-04-21 19:09:07 +0800

    update AdminController.java.

--- una-admin/src/main/java/com/lc/una/admin/controller/AdminController.java
+++ una-admin/src/main/java/com/lc/una/admin/controller/AdminController.java
@@ -1,6 +1,7 @@
 package com.lc.una.admin.controller;
 
 import cn.hutool.log.StaticLog;
+import com.lc.una.utils.ResultUtil;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,8 +9,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 管理员表
+ *
  * @author LC
  * @date 2021年4月21日16:38:22
  */
@@ -22,6 +27,10 @@ public class AdminController {
 	@GetMapping("/getList")
 	public String getList(@RequestParam(value = "name", defaultValue = "LC", required = false) String name) {
 		StaticLog.info("...getList...获取管理员列表...");
-		return "hello admin";
+		List<String> userList = new ArrayList<String>();
+		for (int i = 0; i < 10; i++) {
+			userList.add("user-" + i);
+		}
+		return ResultUtil.successWithDataAndMessage( userList, "...getList...获取管理员列表...");
 	}
 }
