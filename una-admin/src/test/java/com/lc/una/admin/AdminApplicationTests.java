523cdeca8cfa366cbfd3ad4260c5351247f47f75
ahviplc <ahlc@sina.cn>
2021-04-21 18:02:29 +0800

    update AdminApplicationTests.java.

--- una-admin/src/test/java/com/lc/una/admin/AdminApplicationTests.java
+++ una-admin/src/test/java/com/lc/una/admin/AdminApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ProviderApplicationTests {
+class AdminApplicationTests {
 
     @Test
     void contextLoads() {
